90b09987df44d25142bd1a769b3bb9744067aa73
Kenneth White <kennival@hotmail.com>
2022-01-11 18:20:15 -0800

    Different selectors, styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[custom-attribute]', e.g. <div custom-attribute>
+  // selector: '.custom-class', e.g. <div class="custom-class">
   template: `
     <app-server></app-server>
     <app-server></app-server>
